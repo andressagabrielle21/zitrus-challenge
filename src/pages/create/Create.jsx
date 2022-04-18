408382df59480dbcba34f1575cfdd4dd82ce1fdc
andressagabrielle21 <andressagss21@gmail.com>
2022-04-18 18:30:03 -0300

    Forms fixed with CEP autocomplete

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -9,18 +9,14 @@ import { Link, useNavigate } from 'react-router-dom';
 export default function CreateUser() {
 
   // Function that loads the CEP automatically
-  const {register, setValue, setFocus, handleSubmit} = useForm();
+  const {register, setValue, setFocus, handleSubmit, formState: { errors }} = useForm();
 
   const usersCollectionReference = collection(db, "users");
 
   //Creating the variables that will handle the new user's information into the DB
   const [newName, setNewName] = useState("");
   const [newCEP, setNewCEP] = useState("");
-  const [newRua, setNewRua] = useState("");
   const [newNumero, setNewNumero] = useState("");
-  const [newBairro, setNewBairro] = useState("");
-  const [newCidade, setNewCidade] = useState("");
-  const [newEstado, setNewEstado] = useState("");
 
   const checkCEP = (e) => {
     const cep = e.target.value.replace(/\D/g, '');
@@ -34,8 +30,9 @@ export default function CreateUser() {
             setValue('cidade', data.localidade);
             setValue('estado', data.uf);
             // After autocomplete these past sections, it focus instantly to the input NÚMERO
-            setFocus('numero');
+            setFocus('numero')
         })
+        
         
     }
     // Error message when the API can't reach the inserted data
@@ -45,9 +42,7 @@ export default function CreateUser() {
   }
 
   //Make the request to the Firestore DB and new user's data inside 
-  const createUserForm = async () => {
-    await addDoc(usersCollectionReference, {name: newName, cep: newCEP, rua: newRua, numero: newNumero, bairro: newBairro, cidade: newCidade, estado: newEstado});
-  };  
+
 
   // Function to redirect to the HOME page once the form is submitted
   let navigate = useNavigate(); 
@@ -55,10 +50,14 @@ export default function CreateUser() {
     let path = `/home`; 
     navigate(path);
   }
-  const onSubmit = (e) => {
-    createUserForm(e);
+  const onSubmit = (data) => {
+    const createUserForm = async () => {
+      await addDoc(usersCollectionReference, {name: newName, cep: newCEP, rua: data.rua , numero: newNumero, bairro: data.bairro, cidade: data.cidade, estado: data.estado});
+    };  
+    createUserForm();
+    console.log(data);
     alert(newName + " adicionado com sucesso!")
-    routeChange(e);
+    routeChange(data);
   }    
 
   return (
@@ -69,14 +68,17 @@ export default function CreateUser() {
               <Link to="/home"><Button className='closeButton'>X</Button></Link>
             </div>
                   <Form onSubmit={handleSubmit(onSubmit)}> 
-                    <input type="text" placeholder='Digite seu nome' onChange={(e) => {setNewName(e.target.value);}}></input>
+                    <input type="text" placeholder='Digite seu nome' value={newName} {...register("name", {required: true})} onChange={(e) => {setNewName(e.target.value);}}></input>
                     <input type="number" placeholder='Digite seu CEP' {...register("cep")} onBlur={checkCEP} onChange={(e) => {setNewCEP(e.target.value);}}></input>
-                    <input type="text" placeholder='Rua (Logradouro)' {...register("rua")} onChange={(e) => {setNewRua(e.target.value);}}></input>
-                    <input type="number"  placeholder='Número' {...register("numero")} onChange={(e) => {setNewNumero(e.target.value);}}></input>
-                    <input type="text" placeholder='Bairro' {...register("bairro")} onChange={(e) => {setNewBairro(e.target.value);}}></input>
-                    <input type="text" placeholder='Cidade' {...register("cidade")} onChange={(e) => {setNewCidade(e.target.value);}}></input>
-                    <input type="text" placeholder='Estado' {...register("estado")} onChange={(e) => {setNewEstado(e.target.value);}}></input>
+                    <input type="text" placeholder='Rua (Logradouro)' {...register("rua")} ></input>
+                    <input type="number"  placeholder='Número' value={newNumero} {...register("numero", {required: true})} onChange={(e) => {setNewNumero(e.target.value);}}></input>
+                    <input type="text" placeholder='Bairro' {...register("bairro")} ></input>
+                    <input type="text" placeholder='Cidade' {...register("cidade")}></input>
+                    <input type="text" placeholder='Estado' {...register("estado")}></input>
 
+                    {(errors.newName, errors.newNumero) && (
+                    <div className="form_error">All form values are required to submit.</div>
+                    )}
                     <Button>Salvar</Button>
                   </Form>
           </CreatePage>
